3f9b5631227e01c596486a8961e51a4bebe5eba4
Sebastian.Valdivia <se.valdiviae@duocuc.cl>
2025-06-30 12:12:02 -0400

    feat: Complete Swagger documentation and fix CourseQuiz infrastructure

--- ms-grades/src/main/java/com/edutech/grades/repository/CourseQuizRepository.java
+++ ms-grades/src/main/java/com/edutech/grades/repository/CourseQuizRepository.java
@@ -16,18 +16,31 @@ public interface CourseQuizRepository extends JpaRepository<CourseQuiz, Integer>
     /**
      * Find all quizzes for a specific course
      */
+    List<CourseQuiz> findByCourseId(Integer courseId);
     List<CourseQuiz> findByCourseIdOrderByCreatedAtAsc(Integer courseId);
     List<CourseQuiz> findByCourseIdOrderByCreatedAtDesc(Integer courseId);
     
     /**
      * Find quizzes by type
      */
+    List<CourseQuiz> findByQuizType(String quizType);
     List<CourseQuiz> findByQuizTypeOrderByCreatedAtAsc(String quizType);
     List<CourseQuiz> findByQuizTypeOrderByCreatedAtDesc(String quizType);
     
     /**
      * Find quizzes by course and type
      */
+    List<CourseQuiz> findByCourseIdAndQuizType(Integer courseId, String quizType);
     List<CourseQuiz> findByCourseIdAndQuizTypeOrderByCreatedAtAsc(Integer courseId, String quizType);
     List<CourseQuiz> findByCourseIdAndQuizTypeOrderByCreatedAtDesc(Integer courseId, String quizType);
+    
+    /**
+     * Count methods
+     */
+    long countByCourseId(Integer courseId);
+    
+    /**
+     * Existence check methods
+     */
+    boolean existsByCourseId(Integer courseId);
 }
